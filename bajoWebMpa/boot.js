dd206292ce01ee1e0689ded56db6d921ca9069cc
Ardhi Lukianto <ardhi@lukianto.com>
2023-09-09 20:58:50 +0700

    Updates

--- bajoWebMpa/boot.js
+++ bajoWebMpa/boot.js
@@ -1,18 +1,14 @@
 import handleBooks from '../lib/book/index.js'
-import buildIndex from '../lib/build-index.js'
-import buildBooks from '../lib/build-books.js'
 import markdown from '../lib/markdown.js'
 
 async function boot (ctx) {
   const { getConfig } = this.bajo.helper
   const cfg = getConfig('bajoWebBook')
   const { prefix } = cfg
-  await buildIndex.call(this)
   await markdown.call(this)
   await ctx.register(async (childCtx) => {
-    await handleBooks.call(this, ctx)
+    await handleBooks.call(this, childCtx)
   }, { prefix })
-  buildBooks.call(this)
 }
 
 export default boot
