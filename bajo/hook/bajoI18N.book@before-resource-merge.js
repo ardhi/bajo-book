09f49ceef280b6da6ba6eeb7c4d0c7fe3f044b99
Ardhi Lukianto <ardhi@lukianto.com>
2023-10-14 20:46:19 +0700

    Dynamic schema

--- bajo/hook/bajoI18N.book@before-resource-merge.js
+++ bajo/hook/bajoI18N.book@before-resource-merge.js
@@ -1,11 +1,10 @@
-async function beforeResourceMerge (plugin, lng, content) {
+async function beforeResourceMerge (lng, content) {
   const { eachPlugins, readConfig, importPkg } = this.bajo.helper
   const { merge } = await importPkg('lodash-es')
-  if (plugin !== 'bajoBook') return
   await eachPlugins(async function ({ file, plugin, dir }) {
     const item = await readConfig(file)
     merge(content, item)
-  }, { glob: 'book/**/*.json' })
+  }, { glob: 'book/**/*.json', ns: 'bajoBook' })
 }
 
 export default beforeResourceMerge
